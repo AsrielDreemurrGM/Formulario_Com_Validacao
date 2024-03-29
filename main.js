b1ed40f7b0754dceeea117cf3b7f75116fda12a4
AsrielDreemurrGM <eduardo.motta.980315@gmail.com>
2024-03-29 03:31:49 -0300

    Adicionado Mensagem de Erro

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-const form = document.getElementById('formulario')
+const form = document.getElementById('formulario');
+const valorValida = document.getElementById('valorA');
+var formEValido = false;
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-})
+//NÃO FUNCIONA - ARRUMAR
+// function validaForm(valor) {
+//     const valorB = form.getElementById('ValorB');
+//     return valor < valorB;
+// }
+
+
+// *form.addEventListener('submit', function(e) {
+//     e.preventDefault();
+
+//     formEValido = validaForm(valorValida.value)
+//     if (formEValido) {
+//         valorA.style.border = '1px solid red';
+//         document.querySelector('msg-erro').style.display = 'block';
+//     }
+// }) Ctrl + K, Ctrl + U - remover comentário
