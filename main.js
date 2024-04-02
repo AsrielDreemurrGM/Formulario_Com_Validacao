caa4bf4f8c9843d6917ac22c3f0724421efc8863
AsrielDreemurrGM <eduardo.motta.980315@gmail.com>
2024-04-02 02:40:55 -0300

    Criado Mensagem de Acerto e Código JavaScript para Validação do Formulário

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 const form = document.getElementById('formulario');
-const valorValida = document.getElementById('valorA');
+
+var valorA = document.getElementById('numberA');
+var valorB = document.getElementById('numberB');
 var formEValido = false;
 
-//NÃO FUNCIONA - ARRUMAR
-// function validaForm(valor) {
-//     const valorB = form.getElementById('ValorB');
-//     return valor < valorB;
-// }
+function validaForm(primValor, segValor) {
+    numeroA = parseFloat(primValor);
+    numeroB = parseFloat(segValor);
+    return !isNaN(numeroA) && !isNaN(numeroB) && numeroA < numeroB;
+}
 
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
 
-// *form.addEventListener('submit', function(e) {
-//     e.preventDefault();
+    formEValido = (validaForm(valorA.value, valorB.value))
 
-//     formEValido = validaForm(valorValida.value)
-//     if (formEValido) {
-//         valorA.style.border = '1px solid red';
-//         document.querySelector('msg-erro').style.display = 'block';
-//     }
-// }) Ctrl + K, Ctrl + U - remover comentário
+    if (formEValido) {
+        document.querySelector(".msg-acerto").style.display = 'block';
+        valorA.style.border = '2px solid green';
+        valorB.style.border = '2px solid green';
+    } else {
+        document.querySelector(".msg-erro").style.display = 'block';
+        valorA.style.border = '2px solid red';
+    }
+})
+
+function atualizarValido() {
+    formEValido = (validaForm(valorA.value, valorB.value));
+
+    if (formEValido) {
+        document.querySelector(".msg-erro").style.display = 'none';
+        valorA.style.border = '';
+    } else {
+        document.querySelector(".msg-erro").style.display = 'block';
+        valorA.style.border = '2px solid red';
+        valorB.style.border = '';
+        document.querySelector(".msg-acerto").style.display = 'none';
+        valorA.style.border = '';
+    }
+}
+
+valorA.addEventListener('input', atualizarValido);
+valorB.addEventListener('input', atualizarValido);
